4b1ac05dd41f47cd237a01898021dab7aadb8609
hoteaklee <cc18ht@naver.com>
2023-03-02 16:45:53 +0900

    대학생 등록 폼 작성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
+import EnrollmentForm from "./components/EnrollmentForm";
 
 const App = () => {
-    const ClickHandler = () => {
-        alert('Starting Ract!!')
-    };
     return (
-        <div>
-            <button type='button' onClick={ClickHandler}>Click React</button>
-        </div>
+        <EnrollmentForm />
     );
 };
 
