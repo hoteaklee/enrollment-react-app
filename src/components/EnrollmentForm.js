5f35edb27f57507d1f053f4d134439664a073300
hoteaklee <cc18ht@naver.com>
2023-03-03 14:50:40 +0900

    UI 변경, 입력항목에 email 추가

--- src/components/EnrollmentForm.js
+++ src/components/EnrollmentForm.js
@@ -3,9 +3,13 @@ import { useState } from 'react';
 import '../App.css';
 
 const EnrollmentForm = (props) => {
-    // 폼에 입력한 이름/성을 기억히기 위해서 state형 변수 선언
+    // 폼에 입력한 이름/성/이메일을 기억히기 위해서 state형 변수 선언
+    // onChange 이벤트 발생시 입력한 이름/성/이메일을
+    // firstName, lastName, email 변수에 저장
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+
     // state형 변수에 저장된 이름/성을 환영메세지로 출력하기 위해 선언
     const [welcomeMessage, setWelcomeMessage] = useState("");
     const [msgStyle, setMsgStyle] = useState("redOne");
@@ -20,30 +24,51 @@ const EnrollmentForm = (props) => {
         // 상위 컴퍼넌트의  seats 변수를 조작함
         props.setUpdateSeats( props.currentSeat - 1 ); // 참여가능 인원수 감수
             setMsgStyle('message');
-            msg =`환영합니다. ${firstName} ${lastName}`
+            msg =`환영합니다. ${firstName} ${lastName}님!`+
+                `${email}로 등록관련 정보를 발송했드렸습니다.`;
         }
         setWelcomeMessage(msg);
         e.preventDefault();  //submit 기능 전파 중지
     };
+    const hanldeInputChang = (setInput, e) => {
+        setInput(e.target.value);
+    };
     return(
         <div>
+            <div className="enrolContainer">
             <form className="enrolForm" onSubmit={handleSubmit}>
-        <h1> {props.chosenProgram} 대학생 등록 양식</h1>
-                <div><label>First Name</label>
-                    {/* onblur 이벤트 발생시 입력한  이름/성은
-                    firstName, ladtName변수에 저장 */}
-                <input type="text" name="fname"
-                onBlur={(e)=>setFirstName(e.target.value)}/></div>
-
-                <div><label>Last Name</label>
-                    <input type="text" name="lname"
-                    onBlur={(e)=>setLastName(e.target.value)}/></div>
-                <div><button type="submit">등록하기</button></div>
+                <ul className="ulEnrol">
+                    <li>
+                        <label htmlFor="FirstName"></label>
+                        <input type="text" id="FirstName" name="firstName"
+                               className="inputFields" placeholder="FirstName"
+                               value={firstName} onChange={e => hanldeInputChang(setFirstName,e)} />
+                    </li>
+                    <li>
+                        <label htmlFor="LastName"></label>
+                        <input type="text" id="LastName" name="lastName"
+                               className="inputFields" placeholder="LastName"
+                               value={lastName} onChange={e => hanldeInputChang(setLastName,e)} />
+                    </li>
+                    <li>
+                        <label htmlFor="Email"></label>
+                        <input type="text" id="Email" name="Email"
+                               className="inputFields" placeholder="Email"
+                               value={email} onChange={e => hanldeInputChang(setEmail,e)} />
+                    </li>
+                    <li id="center-btn">
+                        <button type="submit" id="btnEnrol" name="enrol"
+                                onClick={handleSubmit}>등록하기</button>
+                    </li>
+                    <li>
+                        <label id="studentMsg" className={msgStyle}>
+                            {welcomeMessage}
+                        </label>
+                    </li>
+                </ul>
             </form>
 
-            <label id="studentMsg" className={msgStyle}>
-                {welcomeMessage}
-            </label>
+            </div>
         </div>
     );
 };
